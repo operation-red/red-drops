800eb44b2675c5c69dd7fbe6d0317bd1b2da5758
Operation Red <operation.red.llc@gmail.com>
2012-07-26 11:19:53 -0400

    Added SET_STATE_IMAGES Macro
    Added SET_STATE_IMAGES Macro

--- macros.h
+++ macros.h
@@ -16,3 +16,23 @@
  *   }
  */
 #define IS_DEVICE_PHONE [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone
+
+/* 
+ * Name: SET_STATE_IMAGES
+ * Purpose: Simplify setting state images for normal and highlighted(pressed)
+ * Availability: iOS 2.0 and later
+ * Why: Reduces two lines of code into one macro call.
+ *      Intended for use in a for loop.
+ *
+ * Example: 
+ *   UIView *views = [self.view viewWithTag:1000];
+ *   for(UIView *view in views.subviews) {
+ *     // skip any tags set to -1 denoting non image buttons
+ *     if([view isKindOfClass:[UIButton class]] && view.tag > 0) {
+ *       SET_STATE_IMAGES((UIButton *) view, view.tag);
+ *     }
+ *   } 
+ */
+#define SET_STATE_IMAGES(dButton, dFileName)\
+  [dButton setImage:[UIImage imageNamed:[NSString stringWithFormat:@"%i.png", dFileName]] forState:UIControlStateNormal];\
+  [dButton setImage:[UIImage imageNamed:[NSString stringWithFormat:@"%i-hl.png", dFileName]] forState:UIControlStateHighlighted]
